dd1c4cc47d3b3c0f699e1c2e62f7e82e01079cbb
Marko Dujović <dujovicm@outlook.com>
2022-07-05 21:56:52 +0200

    Remove render layers

--- src/main/java/com/gamma1772/additionalbarsbop/ClientABBOP.java
+++ src/main/java/com/gamma1772/additionalbarsbop/ClientABBOP.java
@@ -27,51 +27,51 @@ public class ClientABBOP {
 	public static void clientSetup (final FMLClientSetupEvent event) {
 		AdditionalBars.LOGGER.debug("[ADDITIONAL BARS BOP] Doing client stuff...");
 
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.FIR_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.REDWOOD_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CHERRY_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.MAHOGANY_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.JACARANDA_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.PALM_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.WILLOW_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.DEAD_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.MAGIC_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.UMBRAN_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HELLBARK_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_FIR_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_REDWOOD_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_CHERRY_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_MAHOGANY_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_JACARANDA_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_PALM_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_WILLOW_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_DEAD_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_MAGIC_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_UMBRAN_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_HELLBARK_BARS.get(), RenderType.cutout());
-
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_FIR_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_REDWOOD_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CHERRY_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_MAHOGANY_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_JACARANDA_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_PALM_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_WILLOW_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_DEAD_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_MAGIC_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_UMBRAN_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_HELLBARK_BARS.get(), RenderType.cutout());
-
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_FIR_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_REDWOOD_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_CHERRY_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_MAHOGANY_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_JACARANDA_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_PALM_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_WILLOW_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_DEAD_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_MAGIC_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_UMBRAN_BARS.get(), RenderType.cutout());
-		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_HELLBARK_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.FIR_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.REDWOOD_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CHERRY_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.MAHOGANY_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.JACARANDA_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.PALM_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.WILLOW_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.DEAD_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.MAGIC_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.UMBRAN_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HELLBARK_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_FIR_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_REDWOOD_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_CHERRY_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_MAHOGANY_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_JACARANDA_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_PALM_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_WILLOW_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_DEAD_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_MAGIC_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_UMBRAN_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.CROSSED_HELLBARK_BARS.get(), RenderType.cutout());
+//
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_FIR_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_REDWOOD_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CHERRY_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_MAHOGANY_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_JACARANDA_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_PALM_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_WILLOW_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_DEAD_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_MAGIC_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_UMBRAN_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_HELLBARK_BARS.get(), RenderType.cutout());
+//
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_FIR_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_REDWOOD_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_CHERRY_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_MAHOGANY_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_JACARANDA_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_PALM_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_WILLOW_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_DEAD_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_MAGIC_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_UMBRAN_BARS.get(), RenderType.cutout());
+//		ItemBlockRenderTypes.setRenderLayer(ABBlocksBOP.HORIZONTAL_CROSSED_HELLBARK_BARS.get(), RenderType.cutout());
 	}
 }
